ae7280ce6bd16dc7d43b1ae9033e2cc9dc16e6bd
cristian <scarlat.cristian.gabriel@gmail.com>
2023-12-19 20:22:52 +0200

    create Blog class and main routes

---
+++ blog.py
@@ -0,0 +1,6 @@
+class Blog:
+    def __init__(self, post_id, title, subtitle, body):
+        self.id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
