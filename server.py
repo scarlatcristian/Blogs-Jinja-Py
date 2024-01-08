839fe8d4d62feff00e1c742d24fe4517422393fb
scarlat <scarlat.cristian.gabriel@gmail.com>
2024-01-08 20:14:50 +0200

    create endpoint for blogs and render them

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+url_blogs = 'https://api.npoint.io/b8fb46730f57568d6b43'
+blog_data = requests.get(url_blogs).json()
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', blogs=blog_data)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
